172b517eeff7aa3e414834f175893b5c2401ec84
Otniel Aguilar <otnieel.aguilar@gmail.com>
2015-01-20 13:37:02 -0600

    Pre-homework almost done, just missing Tk window for displaying image.

--- Homeworks/prehw/simple_image_manipulation.py
+++ Homeworks/prehw/simple_image_manipulation.py
@@ -1,9 +1,27 @@
 import Image
+import sys
 
-image = Image.open('/home/otniel/Pictures/mas-mezcla-maistro.jpg');
-print image.mode
+if len(sys.argv) != 3:
+    print "Usage -> simple_image_manipulation.py FILENAME_INPUT FILENAME_OUTPUT"
+    exit()
+
+filename_input = sys.argv[1] 
+filename_output = sys.argv[2]
+
+image = Image.open(filename_input);
 
 if image.mode != 'RGBA':
     image = image.convert('RGBA')
 
-image.save('/home/otniel/Pictures/maistro.jpg')
+width = image.size[0]
+height = image.size[1]
+
+pixels = image.load()
+
+for x_axis in range(width-1):
+    for y_axis in range(height-1):
+        red,green,blue,alpha = pixels[x_axis, y_axis]
+        new_pixel =  blue,green,red,alpha
+        pixels[x_axis, y_axis] = new_pixel
+
+image.save(filename_output)

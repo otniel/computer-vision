c0b4ff469c56715ee0019a2f230820013c2ad259
Otniel Aguilar <otnieel.aguilar@gmail.com>
2015-04-06 18:23:17 -0500

    Some refactoring to MiniPaint

--- Lab/minipaint/minipaint.py
+++ Lab/minipaint/minipaint.py
@@ -4,60 +4,63 @@ from Tkinter import Tk, Canvas, Frame, BOTH, NW, Label, Text, Button
 from ttk import Style
 from utils.tools import generate_random_color, convert_rgb_to_hex
 
+DEFAULT_WIDTH = 800
+DEFAULT_HEIGHT = 600
+DEFAULT_COLOR = '#1A735F'
+
 
 class MiniPaint(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
-
         self.parent = parent
-
         self.initUI()
 
-    def get_pixel(self, event):
-        self.draw_point(event.x, event.y)
-
-    def draw_point(self, x, y):
-        print x, y
-        point = convert_rgb_to_hex(generate_random_color())
-        self.canvas.create_rectangle((x - 2, y - 2, x + 2, y + 2), fill='#000000')
-
-        # minipaint.rectangle(, fill=point)
-
     def initUI(self):
-        self.parent.title("Windows")
-        self.style = Style()
-        self.style.theme_use("classic")
-        self.pack(fill=BOTH, expand=1)
+        self.setup_window()
+        self.setup_canvas()
+        self.setup_buttons()
 
+    def setup_window(self):
+        self.parent.title("Mini Paint")
+        self.pack(fill=BOTH, expand=1)
+        self.setup_grid_padding()
+
+        lbl = Label(self, text="Draw whatever you want!")
+        lbl.grid(sticky=W, pady=4, padx=5)
+
+    def setup_grid_padding(self):
         self.columnconfigure(1, weight=1)
         self.columnconfigure(3, pad=7)
         self.rowconfigure(3, weight=1)
         self.rowconfigure(5, pad=7)
 
-        lbl = Label(self, text="Draw whatever you want!")
-        lbl.grid(sticky=W, pady=4, padx=5)
+    def setup_canvas_size(self):
+        self.canvas_width = self.canvas_image.width() + 20
+        self.canvas_height = self.canvas_image.height() + 20
 
-        self.img = Image.new("RGB", (800, 600), '#1A735F')
-        self.tatras = ImageTk.PhotoImage(self.img)
+    def setup_canvas_image(self):
+        self.image = Image.new("RGB", (DEFAULT_WIDTH, DEFAULT_HEIGHT), '%s' % DEFAULT_COLOR)
+        self.canvas_image = ImageTk.PhotoImage(self.image)
+        self.setup_canvas_size()
 
-        self.canvas = Canvas(self, width=self.img.size[0] + 20, height=self.img.size[1] + 20, cursor="cross")
-        self.canvas.create_image(10, 10, anchor=NW, image=self.tatras)
-        self.canvas.grid(row=1, column=0, columnspan=2, rowspan=4,
-                         padx=5, sticky=E + W + S + N)
+    def setup_canvas(self):
+        self.setup_canvas_image()
+        self.canvas = Canvas(self, width=self.canvas_width, height=self.canvas_height)
+        self.canvas.create_image(10, 10, anchor=NW, image=self.canvas_image)
+        self.canvas.grid(row=1, column=0, columnspan=2, rowspan=4, padx=5, sticky=E + W + S + N)
+        self.canvas.bind("<ButtonPress-1>", self.draw_point)
 
-        self.canvas.bind("<ButtonPress-1>", self.get_pixel)
+    def draw_point(self, event):
+        self.canvas.create_rectangle((event.x - 2, event.y - 2, event.x + 2, event.y + 2), fill='#000000')
 
-        abtn = Button(self, text="Point")
-        abtn.grid(row=1, column=3)
+    def setup_buttons(self):
+        point_button = Button(self, text="Point")
+        point_button.grid(row=1, column=3)
+        clear_button = Button(self, text="Clear", command=self.clear_canvas)
+        clear_button.grid(row=5, column=0, padx=5)
 
-        cbtn = Button(self, text="Close")
-        cbtn.grid(row=1, column=4, pady=5, padx=5)
-
-        hbtn = Button(self, text="Clear")
-        hbtn.grid(row=5, column=0, padx=5)
-
-        # obtn = Button(self, text="OK")
-        # obtn.grid(row=5, column=3)
+    def clear_canvas(self):
+        self.setup_canvas()
 
 
 def main():
